6f788ff4d810d5c261404a64088f33f47a27e410
ldavi8533 <ldavi8533@gmail.com>
2022-05-19 20:23:06 -0500

    Inicio do projeto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
